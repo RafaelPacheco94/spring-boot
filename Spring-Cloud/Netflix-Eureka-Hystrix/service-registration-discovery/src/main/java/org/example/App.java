a9ae53639a691c1a3e24d4ae88e220aa22f30421
Rafael Pacheco <rafaelsousa177@gmail.com>
2021-01-25 13:52:26 +0000

    Added Spring Cloud Configuration Server

--- Spring-Cloud/Netflix-Eureka-Hystrix/service-registration-discovery/src/main/java/org/example/App.java
+++ Spring-Cloud/Netflix-Eureka-Hystrix/service-registration-discovery/src/main/java/org/example/App.java
@@ -2,10 +2,12 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableConfigServer
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
